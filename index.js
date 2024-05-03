1ced041da6e6a96bf37a1d1b064f6cef2ae9665b
RehanaMudassar <rehana.mudassar@gmail.com>
2024-05-03 18:56:23 +0500

    Program is Done

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\n      Welcome to Number Guessing      \n\n");
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "usernumber",
+        type: Number,
+        message: "Enter you number between 1-6",
+    }
+]);
+if (answers.usernumber === randomNumber) {
+    console.log("Congratulations Your guess is right");
+}
+else {
+    console.log("Oh Your guess is wrong");
+}
